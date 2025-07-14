ab035b6f1f65b6b7200dacce5c961099812e8612
pooja <kumarip2510@gmail.com>
2025-07-14 21:06:14 +0530

    done

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <div className='flex flex-col items-center justify-center text-center space-y-2 my-32 '>
+      <h1 className='md:text-4xl text-2xl font-semibold'>Never Miss A Blog!</h1>
+      <p className='md:text-lg text-gray-500/70 pb-8'>Subscribe to our newsletter to get the latest updates and articles directly in your inbox.</p>
+      <form className="flex items-center justify-between max-w-2xl w-full md:h-13 h-12">
+        <input className='border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500' 
+            type="text" placeholder='Enter Email' required />
+        <button  type = 'submit'className="md:px-12 px-8 h-full text-white bg-primary/90 hover:bg-primary transition-all
+            cursor:pointer rounded-md rounded-l-none
+        ">
+            Subscribe
+        </button>
+      </form>
+    </div>
   )
 }
 
