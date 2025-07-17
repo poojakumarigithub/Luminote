7f46dfe9e894f48b2e22b17007b6fa810cd08132
pooja <kumarip2510@gmail.com>
2025-07-17 11:11:38 +0530

    imagekit setup

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,4 +1,6 @@
-
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
 export const addBlog = async (req, res) => {
     try {
         const {title , subTitle, description, category, isPublished} = JSON.parse(req.body.blog);
@@ -7,7 +9,32 @@ export const addBlog = async (req, res) => {
         if(!title || !description || !category || !imageFile){
             return res.json({success: false, message: "Please fill all the fields"});
         }
+        const fileBuffer = fs.readFileSync(imageFile.path);
+        //upload image to imagekit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "blogImages",
+        })
+
+        //optimize image url
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {width: '1280'}, // Resize to 1280px width
+                {quality: "auto"},// Automatically adjust quality
+                {format: "webp"} // Convert to WebP format
+            ]
+        });
+
+        const image = optimizedImageUrl;
+
+        //save blog to db
+        await Blog.create({title, subTitle, description, category, image, isPublished});
+
+        res.json({success: true, message: "Blog added successfully"});
+
     } catch (error) {
-        
+        res.json({success: false, message: error.message});
     }
 }
